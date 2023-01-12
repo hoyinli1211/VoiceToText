a0029d2700523001c5c2fdb5cc61dc16dff3263f
hoyinli1211 <hoyinli.r@gmail.com>
2023-01-12 21:58:27 +0800

    Update App.py

--- App.py
+++ App.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import speech_recognition as sr
+import spacy
+from spacy import displacy
 
 def transcribe_audio(audio_file):
     recognizer = sr.Recognizer()
@@ -8,6 +10,12 @@ def transcribe_audio(audio_file):
     transcribed_text = recognizer.recognize_google(audio)
     return transcribed_text
 
+def highlight_entities(text):
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(text)
+    html = displacy.render(doc, style="ent")
+    return html
+
 def main():
     st.set_page_config(page_title="Transcribe Audio", page_icon=":microphone:", layout="wide")
     st.title("Transcribe Audio")
@@ -15,7 +23,10 @@ def main():
     file = st.file_uploader("Upload a .wav file", type=["wav"])
     if file:
         transcribed_text = transcribe_audio(file)
-        st.write("Transcribed Text: ", transcribed_text)
+        entities_html = highlight_entities(transcribed_text)
+        st.write("Transcribed Text: ",transcribed_text)
+        st.write("Entities: ", unsafe_allow_html=True)
+        st.write(entities_html, unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
