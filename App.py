1c9caf1c64768393e5e6da2d58f19fb27a553f9e
hoyinli1211 <hoyinli.r@gmail.com>
2023-01-12 21:51:46 +0800

    Update App.py

--- App.py
+++ App.py
@@ -1,14 +1,21 @@
 import streamlit as st
-import wave
+import speech_recognition as sr
+
+def transcribe_audio(audio_file):
+    recognizer = sr.Recognizer()
+    with sr.AudioFile(audio_file) as source:
+        audio = recognizer.record(source)
+    transcribed_text = recognizer.recognize_google(audio)
+    return transcribed_text
 
 def main():
-    st.set_page_config(page_title="Upload Voice File", page_icon=":microphone:", layout="wide")
-    st.title("Upload Voice File")
+    st.set_page_config(page_title="Transcribe Audio", page_icon=":microphone:", layout="wide")
+    st.title("Transcribe Audio")
 
     file = st.file_uploader("Upload a .wav file", type=["wav"])
     if file:
-        with wave.open(file, "rb") as f:
-            st.write("File Name: ", f.getparams())
+        transcribed_text = transcribe_audio(file)
+        st.write("Transcribed Text: ", transcribed_text)
 
 if __name__ == "__main__":
     main()
