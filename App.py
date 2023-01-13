0969f21b73f7266a17cf7ff6bf0c4e640f377eae
hoyinli1211 <hoyinli.r@gmail.com>
2023-01-13 23:43:25 +0800

    Update App.py

--- App.py
+++ App.py
@@ -18,57 +18,6 @@ def highlight_entities(text):
     html = displacy.render(doc, style="ent")
     return html
 
-def generate_html(audio_src, text):
-    return f"""
-    <audio id="audio" src="{audio_src}" controls></audio>
-    <p id="transcribed-text">{text}</p>
-    <script>
-        var audio = document.getElementById("audio");
-        var transcribedText = document.getElementById("transcribed-text");
-        var words = transcribedText.innerText.split(" ");
-        var wordIndex = 0;
-
-        var synth = window.speechSynthesis;
-        var voices = synth.getVoices();
-        var utterance = new SpeechSynthesisUtterance();
-        utterance.voice = voices[0];
-        utterance.pitch = 1;
-        utterance.rate = 1;
-
-        audio.addEventListener("timeupdate", function(){{
-            var currentTime = audio.currentTime;
-            while (wordIndex < words.length && words[wordIndex].end < currentTime) {{
-                words[wordIndex].element.style.backgroundColor = "white";
-                wordIndex++;
-            }}
-            if (wordIndex < words.length) {{
-                words[wordIndex].element.style.backgroundColor = "yellow";
-            }}
-        }});
-
-        audio.addEventListener("play", function(){{
-            wordIndex = 0;
-            utterance.text = transcribedText.innerText;
-            synth.speak(utterance);
-        }});
-
-        audio.addEventListener("pause", function(){{
-            synth.cancel();
-        }});
-
-        audio.addEventListener("ended", function(){{
-            synth.cancel();
-        }});
-
-        transcribedText.innerHTML = words.map(function(word) {{
-            var element = document.createElement("span");
-            element.innerText = word;
-            element.style.backgroundColor = "white";
-            return element;
-        }});
-    </script>
-    """
-
 def main():
     st.set_page_config(page_title="Transcribe Audio", page_icon=":microphone:", layout="wide")
     st.title("Transcribe Audio")
@@ -77,7 +26,7 @@ def main():
     if uploaded_file is not None:
         transcribed_text = transcribe_audio(uploaded_file)
         entities_html = highlight_entities(transcribed_text)
-        st.markdown(generate_html(uploaded_file, entities_html), unsafe_allow_html=True)
+        st.markdown(entities_html, unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
