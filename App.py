5c567bf242444b06a7cefd53bcf50a01dceaa36e
hoyinli1211 <hoyinli.r@gmail.com>
2023-01-15 13:35:40 +0800

    Update App.py

--- App.py
+++ App.py
@@ -77,7 +77,7 @@ def main():
     if uploaded_file is not None:
         transcribed_text = transcribe_audio(uploaded_file)
         entities_html = highlight_entities(transcribed_text)
-        st.components.v1.html(generate_html(entities_html), height=2000, scrolling=True)
+        st.components.v1.html(generate_html(uploaded_file, entities_html), height=2000, scrolling=True)
 
 if __name__ == "__main__":
     main()
