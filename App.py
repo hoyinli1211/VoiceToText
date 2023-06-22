f9dd76f4f1b5ffa709fe63325c8d5aca1f11b704
hoyinli1211 <hoyinli.r@gmail.com>
2023-06-22 16:53:22 +0800

    Update App.py

--- App.py
+++ App.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import requests
 import io
+import os
 import tempfile
 import zipfile
 from pydub import AudioSegment
@@ -15,7 +16,9 @@ def download_and_extract_model(url, model_path):
 
 @cache
 def download_and_load_vosk_model(model_url, model_path):
-    download_and_extract_model(model_url, model_path)
+    if not os.path.exists(model_path):
+        os.makedirs(model_path)
+        download_and_extract_model(model_url, model_path)
     return load_vosk_model(model_path)
 
 def load_vosk_model(model_path="vosk-model-en"):
@@ -54,30 +57,31 @@ def main():
 
     # Download and load the Vosk model
     model_url = "https://alphacephei.com/kaldi/models/vosk-model-small-en-us-0.15.zip"
-    model_path = "vosk-model-en"
-    vosk_model = download_and_load_vosk_model(model_url, model_path)
+    with tempfile.TemporaryDirectory() as temp_dir:
+        model_path = os.path.join(temp_dir, "vosk-model-en")
+        vosk_model = download_and_load_vosk_model(model_url, model_path)
 
-    option = st.selectbox("Choose the input source", ["Upload", "URL"], index=0)
+        option = st.selectbox("Choose the input source", ["Upload", "URL"], index=0)
 
-    if option == "Upload":
-        audio_file = st.file_uploader("Choose an audio file", type=["mp3", "wav", "ogg", "flac"])
-    else:
-        url = st.text_input("Enter the audio file URL")
-        audio_file = None
-        if url:
-            audio_file = download_audio(url)
+        if option == "Upload":
+            audio_file = st.file_uploader("Choose an audio file", type=["mp3", "wav", "ogg", "flac"])
+        else:
+            url = st.text_input("Enter the audio file URL")
+            audio_file = None
+            if url:
+                audio_file = download_audio(url)
 
-    if audio_file is not None:
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as temp_file:
-            temp_file.write(audio_file.read())
-            audio_file.seek(0)
-            st.audio(temp_file.name)
+        if audio_file is not None:
+            with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as temp_file:
+                temp_file.write(audio_file.read())
+                audio_file.seek(0)
+                st.audio(temp_file.name)
 
-        st.write("Transcribing audio file...")
-        converted_audio_file = convert_audio_to_wav(audio_file)
-        transcript = audio_file_to_text(converted_audio_file, vosk_model)
-        st.write("Transcription:")
-        st.write(transcript)
+            st.write("Transcribing audio file...")
+            converted_audio_file = convert_audio_to_wav(audio_file)
+            transcript = audio_file_to_text(converted_audio_file, vosk_model)
+            st.write("Transcription:")
+            st.write(transcript)
 
 if __name__ == "__main__":
     main()
