bca2e598cd8dc5a19aa8d1803981b782db8a2e63
hoyinli1211 <hoyinli.r@gmail.com>
2023-06-22 14:07:41 +0800

    Update App.py

--- App.py
+++ App.py
@@ -1,25 +1,35 @@
 import streamlit as st
 import streamlit_webrtc as webrtc
-import speech_recognition as sr
-import spacy
-from spacy import displacy
-import base64
 
-spacy.cli.download("en_core_web_sm")
-
-#grant permission to use microphone
+# grant permission to use microphone
 def create_audio_stream():
     return webrtc.webrtc_streamer(
         key="audio",
-        media_stream_constraints={
-            "audio": True,
-            "video": False,
-        }
+        audio=True,
+        video=False,
+        height=300,
+        width=400,
+        on_audio_data=get_audio_data,
+        detection_interval=500,
+        async_processing=True,
     )
+
+def get_audio_data(audio_data):
+    st.session_state.audio_data.append(audio_data)
+
+def playback_audio():
+    if st.session_state.audio_data:
+        audio_data = b''.join(st.session_state.audio_data)
+        st.audio(audio_data, format="audio/wav")
 
 st.title("Microphone Stream")
 
+if "audio_data" not in st.session_state:
+    st.session_state.audio_data = []
+
 stream = create_audio_stream()
 if stream:
-    st.audio(stream)
-
+    st.button("Start Recording")
+    if st.button("Stop Recording"):
+        st.stop()
+        playback_audio()
